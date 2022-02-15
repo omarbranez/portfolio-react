4a55fedaa13a29eca75ecd174438b026b0d5b825
Omar <omar.branez@gmail.com>
2022-02-15 10:55:36 -0500

    add form validations

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -31,17 +31,17 @@ const Contact = () => {
                     <Grid container direction="column">
                         <Grid container direction="row" >
                         <FormControl margin="normal"> 
-                            <TextField variant="outlined" name="name" label="Name" value={values.name} onChange={handleChange}/>
+                            <TextField variant="outlined" required name="name" label="Name" value={values.name} onChange={handleChange}/>
                         </FormControl>
                         <FormControl margin="normal">
-                            <TextField variant="outlined" name="email" label="Email" value={values.email} onChange={handleChange}/>
+                            <TextField variant="outlined" required name="email" label="Email" type="email" value={values.email} onChange={handleChange}/>
                         </FormControl>
                         </Grid>
                         <FormControl margin="normal">
-                            <TextField variant="outlined" name="subject" label="Subject" value={values.subject} onChange={handleChange}/>
+                            <TextField variant="outlined" required name="subject" label="Subject" value={values.subject} onChange={handleChange}/>
                         </FormControl>
                         <FormControl margin="normal">
-                            <TextField variant="outlined" name="message" multiline rows={4} label="Message" value={values.message} onChange={handleChange}/>
+                            <TextField variant="outlined" required name="message" multiline rows={4} label="Message" value={values.message} onChange={handleChange}/>
                         </FormControl>
                         <Box mt={4} display="flex" justifyContent="center" pt={1}>
                                 <Button
